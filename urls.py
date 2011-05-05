fbdb19b99990d73f5b4d087e90134e3ec703f492
Niko Skrypnik <nskrypnik@gmail.com>
2011-05-05 16:21:20 +0300

    Make test for 100M

--- urls.py
+++ urls.py
@@ -24,6 +24,7 @@ def get_map():
     map.connect(None, '/drop/index', controller="controllers", action="drop_index")
     
     map.connect(None, '/insert/100k/{count:\d+}', controller="test_controllers", action="insert_100k")
+    map.connect(None, '/insert/1m/{count:\d+}', controller="test_controllers", action="insert_1m")
     map.connect(None, '/get/item/{uid:\d+}', controller="test_controllers", action="get_item")
     
     if DEBUG:
